90f67f0f7c29a9de5e33b8a3b6bceefad1c9cb19
generic-projects <abelsotovaldez@gmail.com>
2024-01-08 18:56:46 -0600

    fix(translateservice): new arialabeledby translation

--- src/app/services/translate-messages.service.ts
+++ src/app/services/translate-messages.service.ts
@@ -10,6 +10,12 @@ export class TranslateMessagesService {
     private translateService: TranslateService
   ) { }
 
+  /**
+  * Get translation from Translate Service.
+  * @constructor
+  * @param {string} message - Message key to get translation
+  * @returns {string} Translated message
+  */
   getMessage(message_key:string):string{
     let message = ""
     this.translateService.get(message_key).subscribe((res: string) => {
